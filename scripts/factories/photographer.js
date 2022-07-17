6d9fda5676c43e6db9632f01fc44b00459f00fb8
Wandole <58407717+Wandole@users.noreply.github.com>
2022-07-17 17:22:33 +0300

    Adapt photographer factory for photographer main page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,6 +5,37 @@ function photographerFactory(data) {
 
   const profilURL = `./photographer.html?id=${id}`;
 
+  const localisationText = `${city}, ${country}`;
+
+  // Create a container with photographer main details
+  function getUserDetailsDOM() {
+    const container = document.createElement("div");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = name;
+
+    const localisation = document.createElement("p");
+    localisation.textContent = localisationText;
+
+    const taglineElement = document.createElement("blockquote");
+    taglineElement.textContent = tagline;
+
+    container.append(h1);
+    container.append(localisation);
+    container.append(taglineElement);
+
+    return container;
+  }
+
+  // Create the image tag for user profil picture
+  function getUserPictureDOM() {
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.alt = name;
+
+    return img;
+  }
+
   function getUserCardDOM() {
     // Create a card for a photographer presentation
     const article = document.createElement("article");
@@ -12,15 +43,13 @@ function photographerFactory(data) {
     const anchor = document.createElement("a");
     anchor.href = profilURL;
 
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    img.alt = name;
+    const img = getUserPictureDOM();
 
     const h2 = document.createElement("h2");
     h2.textContent = name;
 
     const localisation = document.createElement("p");
-    localisation.textContent = `${city}, ${country}`;
+    localisation.textContent = localisationText;
 
     const taglineElement = document.createElement("blockquote");
     taglineElement.textContent = tagline;
@@ -29,12 +58,24 @@ function photographerFactory(data) {
     priceElement.textContent = `${price}€/jour`;
 
     article.appendChild(anchor);
+
+    // Linking elements
     anchor.appendChild(img);
     anchor.appendChild(h2);
+
+    // Text elements
     article.appendChild(localisation);
     article.appendChild(taglineElement);
     article.appendChild(priceElement);
+
     return article;
   }
-  return { name, picture, getUserCardDOM, profilURL };
+
+  return {
+    name,
+    picture,
+    getUserCardDOM,
+    getUserPictureDOM,
+    getUserDetailsDOM,
+  };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,4 @@
+async function getPhotographerData(photographerId) {}
 async function init() {
   const urlParams = new URL(document.location).searchParams;
   const photographerID = urlParams.get("id");
