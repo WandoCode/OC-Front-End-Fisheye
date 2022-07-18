6919299a882b08b92b32a081deae2f216cb80c8e
Wandole <58407717+Wandole@users.noreply.github.com>
2022-07-18 17:06:41 +0300

    Add media cards in photographer page for pictures

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,5 +1,5 @@
 function mediaFactory(media, photographerName) {
-  const { title, image, likes } = media;
+  let { title, image, likes } = media;
 
   // Create the media path in the DB
   function getMediaPath() {
@@ -8,13 +8,53 @@ function mediaFactory(media, photographerName) {
     return mediaPath;
   }
 
+  // Toogle like icon and nbr of likes
+  function toggleLike(e) {
+    const icon = e.target;
+
+    if (icon.classList.contains("fa-solid")) {
+      icon.classList.replace("fa-solid", "fa-regular");
+      likes += 1;
+
+      console.log(likes);
+      return;
+    }
+    if (icon.classList.contains("fa-regular")) {
+      icon.classList.replace("fa-regular", "fa-solid");
+      likes -= 1;
+      console.log(likes);
+
+      return;
+    }
+  }
   // Create a card for a picture media
   function getCardPictureDOM() {
     const article = document.createElement("article");
+
     const img = document.createElement("img");
     img.src = getMediaPath();
 
+    const detailsContainer = document.createElement("div");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = `${title}`;
+
+    const likesElement = document.createElement("div");
+    likesElement.textContent = `${likes}`;
+
+    const likeIcone = document.createElement("i");
+    likeIcone.classList.add("fa-solid");
+    likeIcone.classList.add("fa-heart");
+    likeIcone.onclick = toggleLike;
+    // Add image to card
     article.append(img);
+
+    // Add details to card (title, nbr likes)
+    article.append(detailsContainer);
+    detailsContainer.append(likesElement);
+    likesElement.append(likeIcone);
+    detailsContainer.append(h2);
+
     return article;
   }
 
